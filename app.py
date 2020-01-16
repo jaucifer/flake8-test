9b28a9c7a07ad904d2a4c7ff1ba471a3f216f95c
Jason Wong <59460466+jaucifer@users.noreply.github.com>
2020-01-16 11:26:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,11 +8,10 @@ https://runnable.com/docker/python/dockerize-your-flask-application
 
 from flask import Flask
 app = Flask(__name__)
-
 @app.route('/getsum')
 def getsum():
     x = 19+2
     return ('sum of 2 and 8 = {}'.format(x))
-    
+
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=5001)
